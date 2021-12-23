5411d2a530220b6d9474744b8ae89c502ed2cd7c
Loris Braga <lorisbraga.work91@gmail.com>
2021-12-23 23:20:50 +0100

    aggiunta contatore

--- src/App.js
+++ src/App.js
@@ -7,15 +7,33 @@ class App extends Component {
 
 state = {
   cards:[
-    {id:0,  immagine: kebab,  nomePiatto:"Piatto Marcio0", prezzo:100},
-    {id:1,  immagine: kebab,  nomePiatto:"Piatto Marcio1", prezzo:101},
-    {id:2,  immagine: kebab,  nomePiatto:"Piatto Marcio2", prezzo:102},
-    {id:3,  immagine: kebab,  nomePiatto:"Piatto Marcio3", prezzo:103},
-    {id:4,  immagine: kebab,  nomePiatto:"Piatto Marcio4", prezzo:104},
-    {id:5,  immagine: kebab,  nomePiatto:"Piatto Marcio5", prezzo:105},
-    {id:6,  immagine: kebab,  nomePiatto:"Piatto Marcio6", prezzo:106}
+    {id:0,  immagine: kebab,  nomePiatto:"Piatto Marcio0", prezzo:100, quantita:0},
+    {id:1,  immagine: kebab,  nomePiatto:"Piatto Marcio1", prezzo:101, quantita:0},
+    {id:2,  immagine: kebab,  nomePiatto:"Piatto Marcio2", prezzo:102, quantita:0},
+    {id:3,  immagine: kebab,  nomePiatto:"Piatto Marcio3", prezzo:103, quantita:0},
+    {id:4,  immagine: kebab,  nomePiatto:"Piatto Marcio4", prezzo:104, quantita:0},
+    {id:5,  immagine: kebab,  nomePiatto:"Piatto Marcio5", prezzo:105, quantita:0},
+    {id:6,  immagine: kebab,  nomePiatto:"Piatto Marcio6", prezzo:106, quantita:0}
   ]
 }
+
+ 
+handleDelete = cardIdToDelete => {
+  //const updatedCards = this.state.cards.filter(card => card.id !== cardIdToDelete)
+  //this.setState(this.cards = updatedCards); oppure
+
+  const cards = this.state.cards.filter(card => card.id !== cardIdToDelete)
+  this.setState({cards}); // --> la const ricavata cards ha lo stesso nome della prop this.state.cards
+}
+
+handleIncrement = card => {
+  const cards = [...this.state.cards];
+  const id = cards.indexOf(card);
+  cards[id] = {...card};
+  cards[id].quantita ++;
+  this.setState({cards});
+}
+
 
   render() {
     return (
@@ -28,9 +46,12 @@ state = {
             {this.state.cards.map(card => (
               <Card 
                 key={card.id}
-                immagine={card.immagine}
-                nomePiatto={card.nomePiatto}
-                prezzo={card.prezzo}
+                card = {card}
+                onDelete = {this.handleDelete}
+                onIncrement = {this.handleIncrement}
+                // immagine={card.immagine}
+                // nomePiatto={card.nomePiatto}
+                // prezzo={card.prezzo}
               ></Card>
 
             ))}
--- src/Components/card.js
+++ src/Components/card.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+
     render() {
         return (
             <>
                 <div className='col'>
                     <div className="card" style={{ width: '18rem', textAlign: 'center' }}>
-                        <img className="card-img-top" src={this.props.immagine} alt="Card image cap" />
+                        <button type="button" onClick={() => this.props.onIncrement(this.props.card)} className="btn btn-primary">
+                            Aggiungi <span className='badge bg-light text-dark'>{this.props.card.quantita}</span>
+                        </button>
+
+                        <img className="card-img-top" src={this.props.card.immagine} alt="Card image cap" />
                         <div className="card-body">
-                            <h5 className="card-title">{this.props.nomePiatto} Rollo</h5>
-                            <p className="card-text">{this.props.prezzo} soldi</p>
-                            <btn className="btn btn-outline-danger">Elimina</btn>
+                            <h5 className="card-title">{this.props.card.nomePiatto} Rollo</h5>
+                            <p className="card-text">{this.props.card.prezzo} soldi</p>
+                            <button onClick={() => this.props.onDelete(this.props.card.id)} className="btn btn-outline-danger">Elimina</button>
                         </div>
                     </div>
                 </div>
